6f842431ce08ceb45781fe75952cc3ee0917f55a
Toa Carney <user.name>
2019-07-03 22:35:16 +1200

    Cart

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const dbURL = "https://superconductor42.github.io/BookDB/booksdb/Books2/"
+const dbURL = "https://superconductor42.github.io/BookDB/booksdb/Books1/"
 var books = []
 var cart = []
 
@@ -28,6 +28,7 @@ class Book {
     this.id = id
     this.price = price
     this.priceGST = (price * 1.15).toFixed(2)
+    this.gstOnly = (price * 0.15).toFixed(2)
     this.imageURI = dbURL + imageURI
   }
 }
@@ -48,16 +49,27 @@ function loadBooksDB() { // Loads the info from books txt file
 }
 
 function addToCart(bookID) {
-  book = books.find((book) => {if (book.id == bookID) {return(book)}})
+  book = books.find((book) => {if (book.id == bookID) {return(book)}}) // locate the book by id
   console.log(book);
+  cart.push(book) // Add to array
+  displayUpdate();
+}
+
+function removeCart(bookID) {
+  book = books.find((book) => {if (book.id == bookID) {return(book)}}) // locate the book by id
+  index = cart.indexOf(book) // Get the index of the book in the array
+  if (index >= 0) { // If item in array,
+    cart.splice(index, 1) // Remove it
+  }
+  displayUpdate();
 }
 
 function displayUpdate() { // Updates the website
-  bookList = document.getElementById('bookList'); // Get the list of books
+  bookList = document.getElementById('bookList') // Get the list of books
+  bookList.innerHTML = ""
   for (var i = 0; i < books.length; i++) {
-    li = document.createElement("LI"); //Create a list item
+    li = document.createElement("LI") //Create a list item
     //Construct a template for the book
-    console.log(books[i].id);
     li.innerHTML = `
     <p>${books[i].title} by ${books[i].author}</p>
     <img src="${books[i].imageURI}"><br>
@@ -67,6 +79,24 @@ function displayUpdate() { // Updates the website
     `
     bookList.appendChild(li); //Add the list item to the HTML doc
   }
+  cartElement = document.getElementById('cart') // Get the element containing the cart
+  cartElement.innerHTML = ""
+  totalPrice = 0
+  for (var i = 0; i < cart.length; i++) {
+    totalPrice += Number(cart[i].price)
+    li = document.createElement("LI")
+    li.innerHTML = `
+    <p>${cart[i].title} by ${cart[i].author}<br>
+    Price: $${cart[i].price} GST: $${cart[i].gstOnly} Full Price: $${cart[i].priceGST}
+    <button onclick="removeCart('${cart[i].id}')">Remove</button>
+    </p>
+    `
+    cartElement.appendChild(li)
+  }
+  p = document.createElement("p")
+  p.innerHTML = `Total price: $${(totalPrice).toFixed(2)} GST: $${(totalPrice * 0.15).toFixed(2)}<br>
+  Grand Total: $${(totalPrice*1.15).toFixed(2)}`
+  cartElement.appendChild(p)
 }
 
 loadBooksDB();
